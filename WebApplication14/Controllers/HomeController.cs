d9d7009e1fc9437b8543b21cecbcd0d1c1b959d3
J'sonn Harris <jsonn.harris@icstars-columbus.org>
2017-10-17 20:25:20 -0400

    almost done

--- WebApplication14/Controllers/HomeController.cs
+++ WebApplication14/Controllers/HomeController.cs
@@ -3,35 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication14.Models;
 
 namespace WebApplication14.Controllers
 {
 	public class HomeController : Controller
 	{
+        AEPDbContext db = new AEPDbContext();
 		public ActionResult Index()
 		{
-			return View();
-		}
+            return View(db.SCORECARD_FACT.ToList());
+        }
 
 		public ActionResult Statistics()
 		{
 			ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View(db.SCORECARD_FACT.ToList());
+        }
 
 		public ActionResult Culture()
 		{
 			ViewBag.Message = "Your contact page.";
-
-			return View();
+            
+			return View(db.SCORECARD_FACT.ToList());
 		}
 
 		public ActionResult Safety()
 		{
 			ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
+            return View(db.SCORECARD_FACT.ToList());
+        }
 	}
 }
